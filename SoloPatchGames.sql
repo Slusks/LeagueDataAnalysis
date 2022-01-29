aa238dc92491a3af31e6c11d326856ad32196809
sam <samslusky@gmail.com>
2022-01-29 12:21:27 -0500

    Update SoloPatchGames.sql

--- SoloPatchGames.sql
+++ SoloPatchGames.sql
@@ -19,3 +19,25 @@ order by patch ASC;
 #select distinct patch from solopatches;
 
 select * from solopatches;
+
+drop table if exists solopatchesWorld;
+create table solopatches as
+select *
+from
+toplanestats
+where league = 'LCS'
+or league = 'NA LCS'
+or league = 'EU LCS'
+or league = 'LEC'
+or league = 'LCK'
+or league = 'LPL'
+or league = 'NA LCS'
+or league = 'NA LCS'
+and patch in
+(select distinct patch
+from
+toplanestats
+where 
+player="Solo")
+and player != 'zig'
+order by patch ASC;
